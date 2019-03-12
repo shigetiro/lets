e179d601b3a75fda9fde111849f44d8b3dff0034
Vipsu <randoomlp@gmail.com>
2019-03-12 09:59:40 +0100

    Update getReplayHandler.py

--- handlers/getReplayHandler.py
+++ handlers/getReplayHandler.py
@@ -1,13 +1,18 @@
 import os
+import sys
+import traceback
 
 import tornado.gen
 import tornado.web
+from raven.contrib.tornado import SentryMixin
 
 from common.log import logUtils as log
 from common.ripple import userUtils
 from common.web import requestsManager
 from constants import exceptions
+from common.constants import mods
 from objects import glob
+from objects import rxscore
 from common.sentry import sentry
 
 MODULE_NAME = "get_replay"
@@ -32,6 +37,10 @@ class handler(requestsManager.asyncRequestHandler):
 			password = self.get_argument("h")
 			replayID = self.get_argument("c")
 
+			isRelaxing = False
+			if int(replayID) < 500000000:
+				isRelaxing = True
+
 			# Login check
 			userID = userUtils.getID(username)
 			if userID == 0:
@@ -41,24 +50,23 @@ class handler(requestsManager.asyncRequestHandler):
 			if userUtils.check2FA(userID, ip):
 				raise exceptions.need2FAException(MODULE_NAME, username, ip)
 
-			# Get user ID
-			replayData = glob.db.fetch("SELECT scores.*, users.username AS uname FROM scores LEFT JOIN users ON scores.userid = users.id WHERE scores.id = %s", [replayID])
+			replayData = glob.db.fetch("SELECT scores{relax}.*, users.username AS uname FROM scores{relax} LEFT JOIN users ON scores{relax}.userid = users.id WHERE scores{relax}.id = %s".format(relax="_relax" if isRelaxing else ""), [replayID])
 
 			# Increment 'replays watched by others' if needed
 			if replayData is not None:
 				if username != replayData["uname"]:
-					userUtils.incrementReplaysWatched(replayData["userid"], replayData["play_mode"])
+					userUtils.incrementReplaysWatched(replayData["userid"], replayData["play_mode"], replayData["mods"])
 
-			# Serve replay
 			log.info("Serving replay_{}.osr".format(replayID))
-			fileName = "{}/replay_{}.osr".format(glob.conf.config["server"]["replayspath"], replayID)
+			fileName = ".data/replays/replay_{}.osr".format(replayID)
 			if os.path.isfile(fileName):
 				with open(fileName, "rb") as f:
 					fileContent = f.read()
 				self.write(fileContent)
 			else:
-				log.warning("Replay {} doesn't exist".format(replayID))
 				self.write("")
+				log.warning("Replay {} doesn't exist.".format(replayID))
+
 		except exceptions.invalidArgumentsException:
 			pass
 		except exceptions.need2FAException:
